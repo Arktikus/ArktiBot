c3aef87dbec571d464df897d168dc8ec7e40144d
Arktikus <ArktikusArkan@gmail.com>
2022-01-06 16:24:08 +0100

    updated files

--- generateImage.js
+++ generateImage.js
@@ -55,7 +55,7 @@ const generateImage = async (member) => {
 
     // draw in to the server
     ctx.font = "40px Roboto"
-    ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50)
+    ctx.fillText(":3", dim.width / 2, dim.height - dim.margin - 50)
 
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
     return attachment
