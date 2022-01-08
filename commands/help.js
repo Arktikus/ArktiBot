9e742dcc0a0005a916e274b71659c44f8a0cdd26
Arktikus <ArktikusArkan@gmail.com>
2022-01-08 15:26:49 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,17 +3,17 @@ module.exports = {
     description: "Helps you!",
     execute(client, message, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#304281')
+        .setColor('#6d00cc')
         .setTitle('Help')
         .setURL('https://twitch.tv/arktikustwtich')
-        .setDescription('Helping for noobs')
+        //.setDescription('Helping for noobs')
         .addFields(
-            {name: 'General Commands', value: '.ping'},
-            {name: 'Social Media', value: '.twitter'}
+            {name: 'General Commands', value: '.help\n.ping'},
+            {name: 'Social Media', value: '.twitter\n.youtube\n.twitch\n.website'}
         )
         .setImage('https://hifi.de/wp-content/uploads/2020/08/anime-streaming-dienste-augen-1100x550.jpg')
-        .setFooter('Hope this helpes you!')
+        .setFooter('Hope this helps you!');
 
-        .send(newEmbed);
+        message.channel.send({embeds: [newEmbed]});
     }
 }
